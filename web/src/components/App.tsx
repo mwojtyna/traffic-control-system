32689fdf0b476b03f418fc2c3bf9b2523b7787ac
Mateusz Wojtyna <w_mateusz@hotmail.com>
2025-04-21 04:14:59 +0200

    feat: add car destination label, add car id tooltip, remove drag ghost image

--- web/src/components/App.tsx
+++ web/src/components/App.tsx
@@ -1,18 +1,23 @@
+import Controls from "./Controls";
 import Display from "./Display";
 
 export default function App() {
     return (
-        <Display
-            state={{
-                N_SR: [{ end: "south" }, { end: "south" }],
-                N_L: [{ end: "east" }, { end: "east" }],
-                S_SR: [{ end: "north" }, { end: "north" }],
-                S_L: [{ end: "west" }, { end: "west" }],
-                E_SR: [{ end: "west" }, { end: "west" }],
-                E_L: [{ end: "south" }, { end: "south" }],
-                W_SR: [{ end: "east" }, { end: "east" }],
-                W_L: [{ end: "north" }, { end: "north" }],
-            }}
-        />
+        <div className="m-2 flex w-full gap-4">
+            <Display
+                state={{
+                    N_SR: [{ end: "south" }, { end: "south" }],
+                    N_L: [{ end: "east" }, { end: "east" }],
+                    S_SR: [{ id: "CUJ", end: "north" }, { end: "north" }],
+                    S_L: [{ end: "west" }, { end: "west" }],
+                    E_SR: [{ end: "west" }, { end: "west" }],
+                    E_L: [{ end: "south" }, { end: "south" }],
+                    W_SR: [{ end: "east" }, { end: "east" }],
+                    W_L: [{ end: "north" }, { end: "north" }],
+                }}
+            />
+
+            <Controls />
+        </div>
     );
 }
--- web/src/components/Display.tsx
+++ web/src/components/Display.tsx
@@ -1,9 +1,11 @@
 import intersectionImgPath from "@/assets/intersection.png?url";
 import carImgPath from "@/assets/car.webp?url";
+import { useEffect, useRef, useState } from "react";
 
 type Route = "north" | "south" | "west" | "east";
 
 type Car = {
+    id: string;
     end: Route;
 };
 
@@ -29,71 +31,82 @@ const leftPercent_W = 17.3;
 
 export default function Display({ state }: ScreenProps) {
     return (
-        <div className="pointer-events-none relative m-2 w-[66%] rounded-xs">
+        <div
+            className={"relative w-[1000px] rounded-xs select-none"}
+            onMouseDown={(e) => e.preventDefault()}
+        >
             <img src={intersectionImgPath} />
 
-            {state.N_SR.map((_, i) => (
+            {state.N_SR.map((car, i) => (
                 <CarImg
                     key={i}
                     topPercent={topPercent_N - i * carHeightPercent}
                     leftPercent={38.8}
                     rotation={90}
+                    car={car}
                 />
             ))}
-            {state.N_L.map((_, i) => (
+            {state.N_L.map((car, i) => (
                 <CarImg
                     key={i}
                     topPercent={topPercent_N - i * carHeightPercent}
                     leftPercent={43.8}
                     rotation={90}
+                    car={car}
                 />
             ))}
-            {state.S_SR.map((_, i) => (
+            {state.S_SR.map((car, i) => (
                 <CarImg
                     key={i}
                     topPercent={topPercent_S + i * carHeightPercent}
                     leftPercent={53.7}
                     rotation={-90}
+                    car={car}
                 />
             ))}
-            {state.S_L.map((_, i) => (
+            {state.S_L.map((car, i) => (
                 <CarImg
                     key={i}
                     topPercent={topPercent_S + i * carHeightPercent}
                     leftPercent={48.7}
                     rotation={-90}
+                    car={car}
                 />
             ))}
-            {state.E_SR.map((_, i) => (
+            {state.E_SR.map((car, i) => (
                 <CarImg
                     key={i}
                     topPercent={40.8}
                     leftPercent={leftPercent_E + i * carWidthPercent}
                     rotation={180}
+                    car={car}
                 />
             ))}
-            {state.E_L.map((_, i) => (
+            {state.E_L.map((car, i) => (
                 <CarImg
                     key={i}
                     topPercent={45.7}
                     leftPercent={leftPercent_E + i * carWidthPercent}
                     rotation={180}
+                    car={car}
                 />
             ))}
-            {state.W_SR.map((_, i) => (
+            {state.W_SR.map((car, i) => (
                 <CarImg
                     key={i}
                     topPercent={55.5}
                     leftPercent={leftPercent_W + i * carWidthPercent}
                     rotation={0}
+                    car={car}
                 />
             ))}
-            {state.W_L.map((_, i) => (
+            {state.W_L.map((car, i) => (
                 <CarImg
                     key={i}
                     topPercent={50.6}
                     leftPercent={leftPercent_W + i * carWidthPercent}
                     rotation={0}
+                    car={car}
                 />
             ))}
         </div>
@@ -104,18 +117,44 @@ type CarImgProps = {
     topPercent: number;
     leftPercent: number;
     rotation: number;
+    car: Car;
 };
 function CarImg(props: CarImgProps) {
+    const divRef = useRef<HTMLDivElement>(null);
+    const pRef = useRef<HTMLParagraphElement>(null);
+    const [divWidth, setDivWidth] = useState(0);
+    useEffect(() => {
+        const listener = () => {
+            if (divRef.current) {
+                setDivWidth(divRef.current.clientWidth);
+            }
+        };
+        if (divRef.current) {
+            setDivWidth(divRef.current.clientWidth);
+        }
+        window.addEventListener("resize", listener);
+        return () => window.removeEventListener("resize", listener);
+    }, []);
+
     return (
-        <img
-            className="absolute"
-            src={carImgPath}
-            width={"7.5%"}
+        <div
+            ref={divRef}
+            className="absolute z-[1] w-[7.5%]"
+            title={props.car.id}
             style={{
                 top: props.topPercent + "%",
                 left: props.leftPercent + "%",
                 rotate: props.rotation + "deg",
             }}
-        />
+        >
+            <p
+                ref={pRef}
+                className="absolute top-[7%] left-[30%] text-center font-mono font-bold text-white"
+                style={{ rotate: -props.rotation + "deg", fontSize: divWidth * 0.3 }}
+            >
+                {props.car.end.at(0)!.toUpperCase()}
+            </p>
+            <img src={carImgPath} />
+        </div>
     );
 }
